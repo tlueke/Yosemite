c467619871f01b37bef14698fb2095155f900bc6
tlueke@gmail.com <tlueke@gmail.com>
2013-07-17 16:30:22 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yosemite::Application.config.secret_key_base = '826624a968861bb6623c1b74366e73454d1cee9560c94cba945605bdb328b9efc813574330f8272fd2d1d08d6d7c7bcd7353c91b9b0d24f021dca20272d1ba25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yosemite::Application.config.session_store :cookie_store, key: '_Yosemite_session'
